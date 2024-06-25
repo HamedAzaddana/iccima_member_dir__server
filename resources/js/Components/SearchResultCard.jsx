af57d10d7f83324949c786fb20acab17a9357e70
HamedAzaddana <fk71jake@gmail.com>
2024-06-25 21:39:12 +0000

    بسثصفقیبل

--- resources/js/Components/SearchResultCard.jsx
+++ resources/js/Components/SearchResultCard.jsx
@@ -3,10 +3,10 @@ export default function SearchResultCard({ info }) {
         <div className="custom-block bg-white shadow-lg mt-5">
             <a href="#">
                 <div className="row">
-                    <div className="col-lg-2 col-md-2 col-sm-2">
+                    <div className="col-lg-2 col-md-2 col-sm-12">
                         <img className="custom-block-img" src={info.logo_corp} alt={info.corp_name} />
                     </div>
-                    <div className="col-lg-7 col-md-7 col-sm-7">
+                    <div className="col-lg-7 col-md-7 col-sm-12">
                         <h5 className="text-primary">{info.corp_name}</h5> <br />
                         <h5 className="text-primary">{info.first_name} {info.last_name}</h5> <br />
                         <h5 className="text-dark">سهامی خاص، تاسیس {info.year_created}</h5> <br />
@@ -18,7 +18,7 @@ export default function SearchResultCard({ info }) {
                            <b> { info.city }</b>
                         </div>
                     </div>
-                    <div className="col-lg-3 col-md-3 col-sm-3">
+                    <div className="col-lg-3 col-md-3 col-sm-12">
                         <div><i className="fa fa-phone"></i> <strong className="custom-block-contact">{info.phone}</strong></div>
                         <div><i className="fa fa-fax"></i><strong className="custom-block-contact">{info.fax}</strong></div>
                         <div><i className="fa fa-globe"></i><strong className="custom-block-contact">{info.website}</strong></div>
