a44e7cfb6ab148949829d1d9c216b360cabfb73b
HamedAzaddana <fk71jake@gmail.com>
2024-07-11 05:04:47 +0330

    fqwef3

--- routes/web.php
+++ routes/web.php
@@ -18,12 +18,34 @@ use Elastic\Elasticsearch\ClientBuilder;
 
 Route::get('/', [HomePageController::class, 'index'])->name('home.index');
 Route::get('/test', function () {
+    // phpinfo();
     // dd(config('database.connections.elasticsearch.hosts'));
 
     // $client = ClientBuilder::create()
     //     ->setHosts([env("ELASTICSEARCH_URL")])
     //     ->setApiKey(env("ELASTICSEARCH_API_KEY"))
     //     ->build();
+
+    echo var_dump(function_exists('oci_connect'));
+    // echo var_dump(ini_get('extension_dir'));
+
+    // dd(getenv("ORACLE_HOME"),getenv("LD_LIBRARY_PATH")); // /opt/oracle/instantclient_23_4
+    // /home/iccima/oracle_db/instantclient_23_4
+    // echo var_dump(getenv("LD_LIBRARY_PATH"));
+
+    // $username = 'your_username';
+    // $password = 'your_password';
+    // $connection_string = 'localhost/XE';
+
+    // $conn = oci_connect($username, $password, $connection_string);
+
+    // if (!$conn) {
+    //     $error = oci_error();
+    //     echo "Oracle connection error: " . $error['message'];
+    //     exit;
+    // }
+
+    // echo "Connected to Oracle!";
 });
 
 
