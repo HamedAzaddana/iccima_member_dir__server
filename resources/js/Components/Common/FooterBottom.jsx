6716d23088c752b94c5af7dba44e7a952dc922c7
HamedAzaddana <fk71jake@gmail.com>
2024-11-07 19:54:42 +0330

    gergerg

--- resources/js/Components/Common/FooterBottom.jsx
+++ resources/js/Components/Common/FooterBottom.jsx
@@ -12,23 +12,21 @@ export default function FooterBottom({ cntOnlines }) {
                 <div className="m-1 p-1">
                     <div className="footer-menu">
                         <div className="row">
-
-                            <div
-                                dangerouslySetInnerHTML={{ __html: footer_title_1 }}
-                                className="col-lg-7 col-md-7 col-sm-12 mt-3  text-dark">
-                            </div>
-
-                            <div className="col-lg-1 col-md-1 col-sm-12 mb-2">
+                            <div className="footer-menu-logo col-lg-1 col-md-1 col-sm-12 mb-2">
                                 <div className="navbar-header">
                                     <center>
                                         <img style={{
-                                            width: "60px",
-                                            height: "60px",
+                                            width: "65px",
+                                            height: "65px",
                                             borderRadius: "20%",
                                             filter: "contrast(180%)",
                                         }} src="/images/iccima_iran.png" alt="" />
                                     </center>
                                 </div>
+                            </div>
+                            <div
+                                dangerouslySetInnerHTML={{ __html: footer_title_1 }}
+                                className="footer-menu-text-1 col-lg-7 col-md-7 col-sm-12 mt-4  text-dark">
                             </div>
                             <div className="col-lg-2 col-md-2 col-sm-12 text-success d-none">
                                 <strong> {_GL["footer.online_users"]}  {cntOnlines} </strong>
